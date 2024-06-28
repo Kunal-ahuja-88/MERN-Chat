e3722a7f47d442505756977cd8e312822785b8e5
Kunal-ahuja-88 <kahujamp4@gmail.com>
2024-06-28 19:47:35 +0530

    connection setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,46 @@
-import { useAutocomplete } from '@mui/material';
+
+
+import { Button, TextField, Typography } from '@mui/material';
 import React from 'react'
 import { useEffect } from 'react';
 import {io} from "socket.io-client"
+import {Container} from "@mui/material"
 
-function App() {
+const handleSubmit = (e) => {
+
+}
+
+const App= ()=>{
+  const socket = io("http://localhost:3000")
+
+  useEffect(() => {
+  socket.on("connect" , () => {
+    console.log("connected",socket.id)
+  })
+
+    socket.on("welcome",(s)=> {
+      console.log(s)
+    })
+
+    return () => {
+      socket.disconnect()
+    }
   
+  },[])
+
   return (
-    <div>App</div>
+   <Container maxWidth = "sm">
+    <Typography variant = "h1" component="div" gutterBottom>
+      Welcome to Chat
+    </Typography>
+
+    <form onSubmit={handleSubmit}>
+      <TextField id = "outlined-basic" label="Outlined" variant="outlined" />
+      <Button variant ="contained" color="primary">Send</Button>
+    </form>
+   </Container>
   )
 }
 
 export default App
+
--- server/app.js
+++ server/app.js
@@ -8,7 +8,15 @@ const port=3000;
 const app = express();
 const server = createServer(app);
 
-const io = new Server(server)
+const io = new Server(server,{
+  cors:{
+    origin:"http://localhost:5173",
+    methods: ["GET","POST"],
+    credentials:true,
+  },
+})
+
+app.use(cors())
 
 app.get("/",(req,res)=> {
   res.send("Hello bhai")
@@ -17,10 +25,19 @@ app.get("/",(req,res)=> {
 io.on("connection",(socket)=> {
   console.log("User connected");
   console.log("Id",socket.id)
+
+  socket.on("message",(data)=> {
+    console.log(data)
+  })
+ 
+  socket.on("disconnect",()=> {
+    console.log("User disconnected",socket.id)
+  })
 })
 
 
 server.listen(port,()=> {
     console.log(`Server is listening on ${port}`);
 });
+
 
