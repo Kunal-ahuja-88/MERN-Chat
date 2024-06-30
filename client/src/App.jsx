7c8423b0800f99bd39d1eeff3d53aa862625a0ec
Kunal-ahuja-88 <kahujamp4@gmail.com>
2024-07-01 00:39:13 +0530

    added join room with multiple people

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,43 @@
 
 
-import { Button, TextField, Typography } from '@mui/material';
+import { Button, Stack, TextField, Typography } from '@mui/material';
 import React, { useEffect, useState,useMemo } from 'react';
 import { io } from "socket.io-client";
 import { Container } from "@mui/material";
 
+
 const App = () => {
-  const socket = useMemo(() => io("http://localhost:3000"),[])
+  const socket = useMemo(() => io("http://localhost:3000",
+    {withCredentials:true}
+  ),[])
 
   const [message, setMessage] = useState("");
+  const [room, setRoom] = useState("");
+  const [socketId, setSocketId] = useState("");
+  const [messages,setMessages] = useState([]);
+  const [roomName,setRoomName] = useState("")
 
   const handleSubmit = (e) => {
     e.preventDefault();  // Corrected the spelling here
-    socket.emit("message", message);
+    socket.emit("message", {message,room});
     setMessage("");
   };
 
+  const joinRoomHandler = (e) => {
+    e.preventDefault()
+    socket.emit("join-room",roomName)
+    setRoomName("")
+  }
+
   useEffect(() => {
     socket.on("connect", () => {
+      setSocketId(socket.id)
       console.log("connected", socket.id);
     });
 
     socket.on("receive-message",(data) => {
       console.log(data)
+      setMessages((messages)=>[...messages,data]);
     })
 
     socket.on("welcome", (s) => {
@@ -40,18 +55,55 @@ const App = () => {
         Welcome to Chat
       </Typography>
 
+      <Typography variant="h2" component="div" gutterBottom>
+         {socketId}
+      </Typography>
+
+      <form onSubmit={joinRoomHandler}>
+        <h5>
+          Join Room
+        </h5>
+        <TextField 
+          value={roomName}
+          onChange={(e) => setRoomName(e.target.value)}
+          id="outlined-basic" 
+          label="Room Name" 
+          variant="outlined" 
+        />
+
+        <Button type="submit" variant="contained" color="primary">
+          Join
+        </Button>
+
+      </form>
+
       <form onSubmit={handleSubmit}>
         <TextField 
           value={message}
           onChange={(e) => setMessage(e.target.value)}
           id="outlined-basic" 
-          label="Outlined" 
+          label="Message" 
+          variant="outlined" 
+        />
+        <TextField 
+          value={room}
+          onChange={(e) => setRoom(e.target.value)}
+          id="outlined-basic" 
+          label="Room" 
           variant="outlined" 
         />
         <Button type="submit" variant="contained" color="primary">
           Send
         </Button>
       </form>
+
+      <Stack>
+        {messages.map((m,i)=> (
+          <Typography key={i} variant="h6" component="div" gutterBottom>
+            {m}
+          </Typography>
+        ))}
+      </Stack>
     </Container>
   );
 };
--- server/app.js
+++ server/app.js
@@ -2,8 +2,10 @@ import express from "express";
 import {Server} from "socket.io";
 import {createServer} from "http";
 import cors from "cors";
+import jwt from "jsonwebtoken"
 
 const port=3000;
+const secretKey="hf8uur3939i3sd"
 
 const app = express();
 const server = createServer(app);
@@ -18,17 +20,45 @@ const io = new Server(server,{
 
 app.use(cors())
 
+
+
+const user = true;
+
+io.use((socket,next)=> {
+  if(user) 
+    next()
+})
+
 app.get("/",(req,res)=> {
   res.send("Hello bhai")
 })
+
+app.get("/login",(req,res)=> {
+  const token = jwt.sign({_id : "fheu38u9eu9weu9"},secretKey)
+ 
+  res.cookie("token",token,
+    {httpOnly:true,
+     secure:true,
+     sameSite:"none"
+    }).json({
+    message:"Login Success"
+    })
+
+
+})
+
 
 io.on("connection",(socket)=> {
   console.log("User connected");
   console.log("Id",socket.id)
 
-  socket.on("message",(data)=> {
-    console.log(data)
-    io.emit("receive-message",data)
+  socket.on("message",({room,message})=> {
+    console.log({room,message})
+    io.to(room).emit("receive-message",message)
+  })
+
+  socket.on("join-room",(room)=> {
+    socket.join(room)
   })
  
   socket.on("disconnect",()=> {
